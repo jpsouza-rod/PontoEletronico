6f5e7f3c2f4f5c4708709389d1d55eba41f7c118
João Paulo Rodrigues <jpsouza.rod@gmail.com>
2022-01-15 21:45:08 -0300

    Add files via upload

---
+++ PontoEletronico/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+public class Funcionario {
+
+  public Funcionario(){
+      Historico = new List<DateTime>();
+  }
+  public List<DateTime> Historico;
+
+  public void NovoPonto () {
+    Historico.Add(DateTime.Now);
+  }
+
+  public void Historico_ponto (){
+    foreach (var ponto in Historico){
+      Console.WriteLine(ponto);
+    }
+  }
+}
+
+class Program {
+  public static void Main (string[] args) {
+    var Joao = new Funcionario();
+    Joao.NovoPonto();
+    Thread.Sleep(5000);
+    Joao.NovoPonto();
+    Thread.Sleep(5000);
+    Joao.NovoPonto();
+    Joao.Historico_ponto();
+  }
+}
